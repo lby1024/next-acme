fc77704b38580dafa78b0ddb9694a0b0ad72abe8
lby <lby@123>
2024-02-05 23:49:23 +0800

    auth

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -17,8 +17,15 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  
+  function updateInvoice(id: any): string | undefined {
+    throw new Error('Function not implemented.');
+  }
+  
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+
   return (
-    <form>
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
